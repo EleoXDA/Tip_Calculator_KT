7c22e40e9dc60e088dddf4ebe44b02d775f92713
Elbay Malik <eleo.666@gmail.com>
2022-02-04 22:34:26 +0100

    android test instrumentation

--- app/src/androidTest/java/eu/eleo/tipcalculatorbygoogle/CalculatorTests.kt
+++ app/src/androidTest/java/eu/eleo/tipcalculatorbygoogle/CalculatorTests.kt
@@ -1,7 +1,14 @@
 package eu.eleo.tipcalculatorbygoogle
 
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.hamcrest.core.StringContains.containsString
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -12,6 +19,12 @@ class CalculatorTests {
     @get:Rule()
     val activity = ActivityScenarioRule(MainActivity::class.java)
     @Test
-    fun calculate_20_percent_tip() {
+    fun calculate_default_tip() {
+        onView(withId(R.id.cost_of_service_edit_text))
+            .perform(typeText("50.00"))
+        onView(withId(R.id.calculate_button))
+            .perform(click())
+        onView(withId(R.id.tip_result))
+            .check(matches(withText(containsString("$10.00"))))
     }
 }
