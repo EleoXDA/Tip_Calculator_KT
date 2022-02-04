411130e6f1ac81671d11bee1848437720a6c1bf7
Elbay Malik <eleo.666@gmail.com>
2022-02-04 22:24:34 +0100

    fixes and styling

--- app/src/androidTest/java/eu/eleo/tipcalculatorbygoogle/CalculatorTests.kt
+++ app/src/androidTest/java/eu/eleo/tipcalculatorbygoogle/CalculatorTests.kt
@@ -1,4 +1,17 @@
 package eu.eleo.tipcalculatorbygoogle
 
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+
 class CalculatorTests {
+    @get:Rule()
+    val activity = ActivityScenarioRule(MainActivity::class.java)
+    @Test
+    fun calculate_20_percent_tip() {
+    }
 }
