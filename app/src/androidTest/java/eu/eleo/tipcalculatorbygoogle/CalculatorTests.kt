10e8c7088dbe5b647517ca07ac67edeaa549c0f5
Elbay Malik <eleo.666@gmail.com>
2022-02-06 17:32:28 +0100

    AndroidTest with different buttons and options - optimization for old devices

--- app/src/androidTest/java/eu/eleo/tipcalculatorbygoogle/CalculatorTests.kt
+++ app/src/androidTest/java/eu/eleo/tipcalculatorbygoogle/CalculatorTests.kt
@@ -1,5 +1,7 @@
 package eu.eleo.tipcalculatorbygoogle
+import androidx.test.espresso.Espresso.closeSoftKeyboard
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.ViewInteraction
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.action.ViewActions.typeText
 import androidx.test.espresso.assertion.ViewAssertions.matches
@@ -21,6 +23,9 @@ class CalculatorTests {
     fun calculate_default_tip() {
         onView(withId(R.id.cost_of_service_edit_text))
             .perform(typeText("49.00"))
+                Thread.sleep(2000)
+        onView(withId(R.id.cost_of_service_edit_text))
+            .perform(closeSoftKeyboard())
         onView(withId(R.id.option_fifteen))
             .perform(click())
         onView(withId(R.id.round_up_switch))
@@ -30,4 +35,8 @@ class CalculatorTests {
         onView(withId(R.id.tip_result))
             .check(matches(withText(containsString("$7.35"))))
     }
+}
+
+private fun ViewInteraction.perform(closeSoftKeyboard: Unit) {
+    //This is to have closeSoftKeyboard function in JUnit test
 }
